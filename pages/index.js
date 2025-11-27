0cfa168fc9d085da716f9be2804951353eb00964
Renan Victor <renanvictorcontact@gmail.com>
2025-11-27 17:47:08 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>“Hello World”</h1>
+    return <h1>Cada pequeno passo que você dá hoje aproxima você do futuro que deseja construir.</h1>
 }
 
 export default Home;
